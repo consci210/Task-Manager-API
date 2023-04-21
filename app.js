2a21e12eca9690356dd2390609753a914f25179e
Helina Ashenafi <Consci210@gmail.com>
2023-04-21 14:36:11 +0300

    added route handler for non existing pages

--- app.js
+++ app.js
@@ -2,9 +2,9 @@
 const express = require('express')
 const app = express() 
 const tasks = require('./routes/tasks')
+const notFound = require('./middleware/not-found')
 const connectDB = require('./db/connect')
 require('dotenv').config()
-
 
 
 // Middleware & Static files 
@@ -15,6 +15,7 @@ app.use(express.json())
 
 // Routes 
 app.use('/api/v1/tasks' , tasks )
+app.use(notFound)
 const port = 3000 
 
 
@@ -23,7 +24,6 @@ const start = async() =>{
     try{
         await connectDB(process.env.MONGO_URI)
         app.listen(port , console.log(`Server started .... listening on port ${port} `))
-
     }catch(err){
         console.log(err)
     }
