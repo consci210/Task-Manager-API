9e188b9620bb8800978e1eac3c7aaef26fea10cf
Helina Ashenafi <Consci210@gmail.com>
2023-04-21 12:15:47 +0300

    connected to Database

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express() 
 const tasks = require('./routes/tasks')
+const connectDB = require('./db/connect')
+require('dotenv').config()
+
 
 
 // Middleware
@@ -18,6 +21,18 @@ app.get('/hello' ,(req,res)=>{
 
 
 app.use('/api/v1/tasks' , tasks )
-
 const port = 3000 
-app.listen(port , console.log(`Server started .... listening on port ${port} `))
+
+
+// Connect to DB then start listening 
+const start = async() =>{
+    try{
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port , console.log(`Server started .... listening on port ${port} `))
+
+    }catch(err){
+        console.log(err)
+    }
+}
+
+start() 
